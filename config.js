9f10d4ff1487f1c12945dabbcb93e47d94d998fc
refilwe-m <refilwe.dev@gmail.com>
2024-08-02 02:25:11 +0200

    hostUrl setup

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ export const config = {
   baseUrl: VITE_LELAPA_HOST_URL,
   apiVersion: VITE_API_VERSION,
   apiKey: VITE_LELAPA_API_KEY,
-  hostUrl: `${VITE_LELAPA_HOST_URL}/api/${VITE_API_VERSION}`,
+  hostUrl: `${VITE_LELAPA_HOST_URL}/${VITE_API_VERSION}`,
 };
