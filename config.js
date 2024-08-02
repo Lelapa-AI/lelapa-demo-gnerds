ae47b1fe4ee3f50feb96860a475776883ae946b4
refilwe-m <refilwe.dev@gmail.com>
2024-08-02 02:24:07 +0200

    .env setup

---
+++ config.js
@@ -0,0 +1,12 @@
+import {
+  VITE_LELAPA_HOST_URL,
+  VITE_API_VERSION,
+  VITE_LELAPA_API_KEY,
+} from "vite:env";
+
+export const config = {
+  baseUrl: VITE_LELAPA_HOST_URL,
+  apiVersion: VITE_API_VERSION,
+  apiKey: VITE_LELAPA_API_KEY,
+  hostUrl: `${VITE_LELAPA_HOST_URL}/api/${VITE_API_VERSION}`,
+};
