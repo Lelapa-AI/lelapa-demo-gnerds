32c3a1921c85a31d964559f6767d0e5b88d30169
refilwe-m <refilwe.dev@gmail.com>
2024-08-06 08:55:54 +0200

    Tailwind fixes

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,3 +1,3 @@
 export const Home = () => {
-  return <div>Home</div>;
+  return <div className="">Home</div>;
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import "./index.css";
 import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({ routeTree });
