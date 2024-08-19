8a11248088f713b9e6689eb9adaec79fa5831596
refilwe-m <refilwe.dev@gmail.com>
2024-08-19 23:38:40 +0200

    Added proper sized icons

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,38 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+      manifest: {
+        name: "Vula Ringa App",
+        short_name: "VulaRinga",
+        description: "Your one stop shop for all your language needs",
+        theme_color: "#161719",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
     }),
   ],
   resolve: {
