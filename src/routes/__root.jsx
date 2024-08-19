fd5d6818a779bfddb961766fa244619618fc95b1
refilwe-m <refilwe.dev@gmail.com>
2024-08-19 12:30:25 +0200

    Theming

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -2,14 +2,15 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 import { MobileNav } from "../components/common";
+import { AppLayout } from "../components";
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <AppLayout>
       <MobileNav />
       <hr />
       <Outlet />
       <TanStackRouterDevtools />
-    </>
+    </AppLayout>
   ),
 });
