d562834a49f6cfeebb5859ded229d4648b7f57f3
refilwe-m <refilwe.dev@gmail.com>
2024-08-19 23:02:10 +0200

    Let's see

--- vite.config.js
+++ vite.config.js
@@ -10,28 +10,9 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
-      manifest: {
-        name: "Your App Name",
-        short_name: "App",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "vite.svg",
-            sizes: "64x64",
-            type: "image/svg",
-          },
-          {
-            src: "vite.svg",
-            sizes: "192x192",
-            type: "image/svg",
-          },
-          {
-            src: "vite.svg",
-            sizes: "512x512",
-            type: "image/svg",
-          },
-        ],
+      theme_color: "#161719",
+      devOptions: {
+        enabled: true,
       },
     }),
   ],
