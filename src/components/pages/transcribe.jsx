b54e6fa96e70e137594ab20bc8a29e038afb5ad7
Refilwe Mashile <refilwe.dev@Refilwes-MBP.lan>
2024-09-04 01:56:00 +0200

    Final Audio Component

--- src/components/pages/transcribe.jsx
+++ src/components/pages/transcribe.jsx
@@ -1,22 +1,12 @@
-import { useState } from "react";
 import { AudioRecorder } from "react-audio-voice-recorder";
 import { PageLayout } from "../templates";
 import { Heading } from "../common";
 
 export const Transcribe = () => {
-  const [recording, setRecording] = useState(null);
-
-  const showRecording = (audio) => {
-    if (!recording) return;
-    console.log(audio);
-    setRecording(audio);
-  };
-
   return (
     <PageLayout hasBack>
       <Heading title="Transcribe" />
       <AudioRecorder
-        onRecordingComplete={showRecording}
         audioTrackConstraints={{
           noiseSuppression: true,
           echoCancellation: true,
