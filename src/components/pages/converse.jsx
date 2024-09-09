72d1e10b92f54ee7be37c34a7feff41f406b6e9c
refilwe-m <refilwe.dev@gmail.com>
2024-09-09 19:48:58 +0200

    Headings

--- src/components/pages/converse.jsx
+++ src/components/pages/converse.jsx
@@ -1,10 +1,9 @@
-import { Heading } from "../common";
+
 import { PageLayout } from "../templates";
 
 export const Converse = () => {
   return (
-    <PageLayout hasBack>
-      <Heading title="Converse" />
+    <PageLayout hasBack title="Converse">
     </PageLayout>
   );
 };
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,1 +1,14 @@
 export const FULLSCREEN_ROUTES = ["/", "/home",'/converse'];
+
+export const LANG_CODES = {
+    "Northern Sotho": "nso_Latn",
+    Afrikaans: "afr_Latn",
+    "Southern Sotho": "sot_Latn",
+    Swati: "ssw_Latn",
+    Tsonga: "tso_Latn",
+    Tswana: "tsn_Latn",
+    Xhosa: "xho_Latn",
+    Zulu: "zul_Latn",
+    English: "eng_Latn",
+    Swahili: "swh_Latn",
+  };
