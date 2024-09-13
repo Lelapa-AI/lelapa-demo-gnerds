05987f55b878d1b6406f1afbc0b827894809ef20
Refilwe Mashile <refilwe.dev@Refilwes-MBP.localdomain>
2024-09-13 13:00:48 +0200

    Translation to fixed

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,19 +1,19 @@
 export const FULLSCREEN_ROUTES = ["/", "/home", "/chat"];
 
 export const LANG_CODES = {
-	"Northern Sotho": "nso_Latn",
-	Afrikaans: "afr_Latn",
-	"Southern Sotho": "sot_Latn",
-	Swati: "ssw_Latn",
-	Tsonga: "tso_Latn",
-	Tswana: "tsn_Latn",
-	Xhosa: "xho_Latn",
-	Zulu: "zul_Latn",
-	English: "eng_Latn",
-	Swahili: "swh_Latn",
+  "Northern Sotho": "nso_Latn",
+  Afrikaans: "afr_Latn",
+  "Southern Sotho": "sot_Latn",
+  Swati: "ssw_Latn",
+  Tsonga: "tso_Latn",
+  Tswana: "tsn_Latn",
+  Xhosa: "xho_Latn",
+  Zulu: "zul_Latn",
+  English: "eng_Latn",
+  Swahili: "swh_Latn",
 };
 
 export const CHAT_MODES = {
-	FROM: "from",
-	TO: "to",
+  FROM: "from",
+  TO: "to",
 };
