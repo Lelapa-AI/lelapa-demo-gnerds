b4e28104d621956d4304fb95c7a5f92d8834657b
Refilwe Mashile <refilwe.dev@Refilwes-MBP.lan>
2024-10-09 06:00:47 +0200

    Clean up

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.i1dnpvsqi6o"
+    "revision": "0.kkdn05cp5i"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
